aad095be62694e25b4366c8967a36f6cb79b7604
Ben <131935756+boleynik@users.noreply.github.com>
2024-12-05 17:18:50 -0500

    menu and nav bar work

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.psu.ist.hcdd340.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         tvLogNumber = findViewById(R.id.tvLogNumber);
         Button btnLogMood = findViewById(R.id.btnLogMood);
         BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        // Initialize the custom Toolbar
+        androidx.appcompat.widget.Toolbar topAppBar = findViewById(R.id.topAppBar);
+        setSupportActionBar(topAppBar); // Set it as the app bar for this activity
 
         // Set button click listener
         btnLogMood.setOnClickListener(new View.OnClickListener() {
@@ -55,5 +62,25 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    // Inflate the menu for the top app bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_app_bar_menu, menu); // Inflate your custom menu
+        return true;
+    }
+
+    // Handle menu item clicks
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about) {
+            // Navigate to the About page
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
